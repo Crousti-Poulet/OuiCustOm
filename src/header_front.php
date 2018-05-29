92cf5d5eb831eb453b143119c0442b7e3b1b3ae2
Crousti-Poulet <el-ouni-mehdi@hotmail.fr>
2018-05-29 11:57:58 +0200

    Création fichier header.php/ Installation Bootstrap/ Rename Base.html.twig -> Base_front.html.twig

---
+++ src/header_front.php
@@ -0,0 +1,39 @@
+<!-- HEADER FRONT -->
+<header class="container-fluid p-0">
+
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark purple">
+    <!-- logo -->
+    <a class="navbar-brand" href="#">LOGO ici</a>
+    <!-- Burger-menu -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <!-- Nav-links -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item">
+          <a class="nav-link" id="nav-link-index" href="index.php">Accueil</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" id="nav-link-login" href="#">>Connexion</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" id="nav-link-contact" href="#">Contact</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" id="nav-link-help" href="#">Aide</a>
+        </li>
+
+      </ul>
+
+    </div>
+
+</nav>
+
+</header>
