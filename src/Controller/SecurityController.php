0b63710aff75ad6ce6986b63518fa7c13c3175d4
mams <mam.raza@gmail.com>
2018-05-31 14:46:39 +0200

    ajout des erreurs de validations

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,10 @@ use Webmozart\Assert\Assert;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -31,9 +35,45 @@ class SecurityController extends Controller
     {
          $user = new User (); // on crée l'utilisateur       
         $form = $this->createFormBuilder($user) // on CREE et CONFIGURE le form grace a createFormBuilder qui sera lié a $user 
-                     ->add('username')     
-                     ->add('location')     
-                     ->add('email', EmailType::class)   
+                     ->add('username',TextType::class,[
+                        'required'    => true,
+                        'constraints' => [
+                            new Length( [
+                                'min' => 3,
+                                'max' => 20,
+                                'minMessage' => "Minimum 3 caractères",
+                                'maxMessage' => "Maximum 20 caractères"
+                            ]),
+                            new NotBlank( [
+                                'message' => "Champs non-rempli" 
+                            ])                                           
+                        ] ,            
+                    ]) 
+                     ->add('location',TextType::class,[
+                        'required'    => true,
+                        'constraints' => [
+                            new Length( [
+                                'min' => 3,
+                                'max' => 20,
+                                'minMessage' => "Minimum 3 caractères",
+                                'maxMessage' => "Maximum 20 caractères"
+                            ]),
+                            new NotBlank( [
+                                'message' => "Champs non-rempli" 
+                            ])                                           
+                        ] ,            
+                    ])     
+                     ->add('email', EmailType::class,[
+                        'required'    => true,
+                        'constraints' => [
+                            new Email( [
+                                'message' => "Format non-valide (monadress@exemple.com)"
+                            ]),
+                            new NotBlank( [
+                                'message' => "Champs non-rempli" 
+                            ])                     
+                        ] ,            
+                    ])   
                      ->add('role', ChoiceType::class,[
                           'expanded' => true,
                           'multiple' => true,
@@ -46,6 +86,18 @@ class SecurityController extends Controller
                         'type' => PasswordType::class,
                         'first_options' => ['label' => 'Entrez le mot de passe'],
                         'second_options' => ['label' => 'Entrez à nouveau le mot de passe'],
+                        'required'    => true,
+                        'constraints' => [
+                            new Length( [
+                                'min' => 8,
+                                'max' => 50,
+                                'minMessage' => "Minimum 3 caractères",
+                                'maxMessage' => "Maximum 50 caractères"
+                            ]),
+                            new NotBlank( [
+                                'message' => "Champs non-rempli" 
+                            ])                                           
+                        ] 
                     ])
                                             
                      ->getForm() ;       // on le RECUPERE   
